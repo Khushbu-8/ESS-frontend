41241ae2e5d636c5ccd6380e0c6bf320a73843cd
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-11-28 09:27:55 +0530

    api add

--- src/adminpages/users/Users.jsx
+++ src/adminpages/users/Users.jsx
@@ -11,6 +11,8 @@ const Users = () => {
 
   function getCookie(name) {
     const value = `; ${document.cookie}`;
+    console.log(value);
+    
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
   }
