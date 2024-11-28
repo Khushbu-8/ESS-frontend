d21c74031a728f42f5213bef21ffc23be6dc6e1a
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-11-28 16:29:19 +0530

    Api

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -24,9 +24,8 @@ const Login = () => {
         })
         
         console.log(response.data);
-        console.log(JSON.stringify(response?.data?.token));
-        
-const {token,success} = result;
+    const token =  JSON.stringify(response?.data?.token);
+      
         if (response.status === 200) {
           localStorage.setItem("token",token)
             navigate("/"); // Corrected 'naviget' to 'navigate'
