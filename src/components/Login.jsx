50b484d67ac13b4e1e539b65b479492bd91a912d
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-11-28 16:24:37 +0530

    Api

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,11 +22,13 @@ const Login = () => {
         }, {
             withCredentials: true, // Important: send cookies with the request
         })
+        const result = response.Json()
         console.log(response.data);
-        console.log(JSON.stringify(response?.data));
+        console.log(JSON.stringify(response?.data?.token));
         
-
+const {token,success} = result
         if (response.status === 200) {
+          localStorage.setItem("token",token)
             navigate("/"); // Corrected 'naviget' to 'navigate'
             console.log("Login Successful...");
         }
