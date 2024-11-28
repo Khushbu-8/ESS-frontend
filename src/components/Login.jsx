f84911644ddd73cd444e0ba1e4ae05f5b4a84370
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-11-28 09:38:10 +0530

    api add

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -77,7 +77,7 @@ const Login = ()  =>{
                     type="text"
                     value={phone}
                     onChange={(e) => setPhone(e.target.value)}
-                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white " placeholder="Email" />
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white " placeholder="phone" />
                   <input
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
