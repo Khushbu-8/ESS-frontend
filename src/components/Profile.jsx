9fa63b0d55e4999327c820b4c195e66ba47d8d57
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-11-29 14:04:14 +0530

    api

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -74,9 +74,9 @@ const Profile = () => {
             <p className='text-center font-semibold mt-3'>{loading && "Data is loading..."}</p>
             <div className="max-w-sm mx-auto bg-white shadow-md rounded-lg overflow-hidden">
                 <div className="p-6">
-                    <h2 className="text-2xl font-bold text-gray-900 mb-2">Name : {data.name}</h2>
-                    <p className="text-gray-700 text-base mb-2">Email: {data.email}</p>
-                    <p className="text-gray-700 text-base">ID: {data._id}</p>
+                    <h2 className="text-2xl font-bold text-gray-900 mb-2">Name : </h2>
+                    <p className="text-gray-700 text-base mb-2">Email: </p>
+                    <p className="text-gray-700 text-base">ID:</p>
                 </div>
 
             </div>
