4d68f6876d33f53576d2565377a8769037eaf862
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-11-29 10:57:16 +0530

    api

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -3,15 +3,14 @@ import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
 
-   const Logout = () =>{
-    const navigete = useNavigate();
-    localStorage.removeItem('token');
-    navigete('/')
-    
+    const navigate = useNavigate()
+    const handleLogout = () => {
+        localStorage.removeItem('token')
+        navigate('/')
    }
   return (
     <div>
-       <button onClick={() => Logout()} className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duretion-300 cursor-pointer">Logout</button>
+       <button onClick={handleLogout} className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duretion-300 cursor-pointer">Logout</button>
     </div>
   )
 }
