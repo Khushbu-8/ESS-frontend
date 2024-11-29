8eed69d99130b778641fa4e85a3e0d00dfe009b9
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-11-29 10:47:55 +0530

    api

--- src/components/Navebar.jsx
+++ src/components/Navebar.jsx
@@ -132,13 +132,12 @@ return() =>{
                                 </svg>
                             </label>
                         </div>
-                        {
-                            auth ? <Logout/> :
+                        
                             <div className="">
                                 <Link to={"/login"} className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duretion-300 cursor-pointer">Login</Link>
                              
                             </div>
-                        }
+                        
                     </div>
                 </div>
             </div>
