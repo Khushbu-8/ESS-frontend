c098ad8cd44167dd7e5c8bc20399e67d98b13036
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-11-30 09:44:03 +0530

    api

--- src/components/UserDropdown.jsx
+++ src/components/UserDropdown.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 function UserDropdown() {
-  const [profile,setProfile] = useState();
+  const [profile,setProfile] = useState("");
   const navigate = useNavigate();
 
   const fetchData = () => {
