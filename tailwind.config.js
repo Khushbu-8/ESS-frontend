08897b03afa09cc759fa416f9abdc9028630b7b3
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-05 19:14:23 +0530

    Update Api

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ export default {
         "orange" : "#e92626",
         "primary" : "#e92626",
         "pink-100" : "#f5f5f5",
-      
+        // "base-100" :"#e6dede"
+        // "red-100" : "#fbf6f0"
       
       },
       colors: {
