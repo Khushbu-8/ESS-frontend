6fa93396839db8a2d7a78f15c6631756ecec5e7e
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-10 18:07:10 +0530

    filter

--- src/Pages/SearchScreen .jsx
+++ src/Pages/SearchScreen .jsx
@@ -10,6 +10,8 @@ import axios from 'axios';
 import ServiceDetail from './ServiceDetail';
 
 const SearchScreen = () => {
+    const token = JSON.parse(localStorage.getItem('token'))
+    const[auth,setAuth] = useState(false)
     const [search, setSearch] = useState('')
     const navigate = useNavigate();
     const [searchResult, setSearchResult] = useState([])
@@ -87,6 +89,15 @@ const handleItemClickLocation = (loc) => {
     setSelectedItem(filtercat); // Set the selected item
 };
 
+useEffect(()=>{
+    if(token){
+        setAuth(true)
+        }else{
+            setAuth(false)
+            }
+
+
+},[token])
 
     if (selectedItem) {
         console.log(selectedItem);  
@@ -201,58 +212,64 @@ const handleItemClickLocation = (loc) => {
                     
                 </div>
             </section>
-            <ServiceDetail selectedItem ={selectedItem} handleItemCaregory = {handleItemCaregory} />
-{/* 
-            <section>
-                <div className="container">
-                    <div className="row">
+            {/* <ServiceDetail selectedItem ={selectedItem} handleItemCaregory = {handleItemCaregory} /> */}
+{
+    auth ? (
+        <section>
+        <div className="container">
+            <div className="row">
 
-                        <div className="col-12 flex flex-wrap">
-                            {
-                                selectedItem ? (
-                                    selectedItem.map((user, i) => {
-                                        return (
-                                            <div key={i} className="col-12 col-md-6 col-xl-3 p-2" onClick={() => handleItemCaregory(user.businessCategory)} style={{ cursor: "pointer" }}>
-                                                <div className="card border-0 bg-base-100 shadow-xl" >
-                                                    <div className='d-flex justify-content-between'>
-                                                        <figure className='rounded-md m-3'>
-                                                            <img src="https://img.daisyui.com/images/profile/demo/2@94.webp" >
+                <div className="col-12 flex flex-wrap">
+                    {
+                        selectedItem ? (
+                            selectedItem.map((user, i) => {
+                                return (
+                                    <div key={i} className="col-12 col-md-6 col-xl-3 p-2" onClick={() => handleItemCaregory(user.businessCategory)} style={{ cursor: "pointer" }}>
+                                        <div className="card border-0 bg-base-100 shadow-xl" >
+                                            <div className='d-flex justify-content-between'>
+                                                <figure className='rounded-md m-3'>
+                                                    <img src="https://img.daisyui.com/images/profile/demo/2@94.webp" >
 
-                                                            </img>
-                                                        </figure>
-                                                        <span className='bg-white rounded-full m-2 shadow-xl w-[30px] h-[30px] d-flex align-items-center justify-content-center '><HiDotsHorizontal /></span>
-                                                    </div>
-                                                    <div className='p-3'>
-                                                        <h2 className=" font-bold">{user.name}</h2>
-                                                        <h5 className=" font-bold">{user.businessCategory}</h5>
-                                                        <h6 className=" font-bold">{user.address}</h6>
+                                                    </img>
+                                                </figure>
+                                                <span className='bg-white rounded-full m-2 shadow-xl w-[30px] h-[30px] d-flex align-items-center justify-content-center '><HiDotsHorizontal /></span>
+                                            </div>
+                                            <div className='p-3'>
+                                                <h2 className=" font-bold">{user.name}</h2>
+                                                <h5 className=" font-bold">{user.businessCategory}</h5>
+                                                <h6 className=" font-bold">{user.address}</h6>
 
-                                                        <p className="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, laborum.</p>
-                                                        <div className="rating rating-sm py-4 d-flex align-items-center">
-                                                        <FaStar className='text-warning'/>
-                                                        <FaStar className='text-warning'/>
-                                                        <FaStar className='text-warning'/>
-                                                        <FaStar className='text-warning'/>
-                                                        <FaStar className='text-warning'/> <span className='ps-2'>rating</span>
-                                                    </div>
-
-                                                    </div>
-                                                </div>
+                                                <p className="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, laborum.</p>
+                                                <div className="rating rating-sm py-4 d-flex align-items-center">
+                                                <FaStar className='text-warning'/>
+                                                <FaStar className='text-warning'/>
+                                                <FaStar className='text-warning'/>
+                                                <FaStar className='text-warning'/>
+                                                <FaStar className='text-warning'/> <span className='ps-2'>rating</span>
                                             </div>
 
-                                        )
-                                    })
-                                ) : (
-                                    <h4>No item Found</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+
                                 )
-                            }
-                       
+                            })
+                        ) : (
+                            <h4>No item Found</h4>
+                        )
+                    }
+               
 
-                        </div>
-                    </div>
                 </div>
-            </section> */}
+            </div>
+        </div>
+    </section>
 
+    ) :(
+        <h5>no result found</h5>
+    )
+}
+           
 
         </>
     )
