d56af995875ce885e2825971d19c43981b0b83cc
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-11 12:06:01 +0530

    slider

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,68 +1,106 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from "../../public/ees-logo.png"
-import {  Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 
+// 
+// for address 
 function Registration() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmpassword, setConfirmpassword] = useState('');
-    const [phone, setPhone] = useState('');
-    const [address, setAddress] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [loading, setLoading] = useState(false)
-    const isAuthenticated = localStorage.getItem("token");
-    const navigete = useNavigate();
-    if (isAuthenticated) {
-      // Redirect to a protected page if already logged in
-      return <Navigate to="/" />;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmpassword, setConfirmpassword] = useState('');
+  const [phone, setPhone] = useState('');
+// for address
+
+
+
+  const [area, setArea] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [country, setCountry] = useState('');
+  const [pincode, setPincode] = useState('');
+  const [address, setAddress] = useState([])
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false)
+  const isAuthenticated = localStorage.getItem("token");
+  const navigete = useNavigate();
+  if (isAuthenticated) {
+    // Redirect to a protected page if already logged in
+    return <Navigate to="/" />;
 
   }
- 
-  
-    const handleSubmits = async (e) => {
-        setLoading(true)
-        e.preventDefault();
-        navigete("/registernext", {state :  { name :name, email :email, password :password,confirmpassword :password, phone:phone,address:address }})
 
-    };
 
-    return (
-        <>
+  const handleSubmits = async (e) => {
+    setLoading(true)
+    e.preventDefault();
+    let newadd = {
+      area,
+      city,
+      state,
+      country,
+      pincode
+    }
+    setAddress(newadd)
+console.log(address,"address");
 
-        <div className="container">
-          <div className="row">
-          <div className=" bg-gray-100 text-gray-900 flex justify-center">
-        <div className=" sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-          <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-            <div className="flex flex-col items-center">
-              <div className="w-full flex-1 mt-8">
-                <div className="flex flex-col items-center">
-                  <div className='mb-12'>
-                    <img src={logo} width={100} />
-                  </div>
-                  <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-green-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
-                    <div className="bg-white p-2 rounded-full">
-                      <svg className="w-4" viewBox="0 0 533.5 544.3">
-                        <path d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z" fill="#4285f4" />
-                        <path d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z" fill="#34a853" />
-                        <path d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z" fill="#fbbc04" />
-                        <path d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z" fill="#ea4335" />
-                      </svg>
+    navigete("/registernext", { state: { name: name, email: email, password: password, confirmpassword: password, phone: phone, address: newadd } })
+
+  };
+
+  useEffect(()=>{
+   
+  },[])
+  return (
+    <>
+
+      <div className="container pt-24">
+        <div className="row mx-auto ">
+          <div className="registerpage bg w-full bg-white p-0">
+            <div className="col-12 d-flex flex-wrap">
+
+              <div className="col-12 col-lg-6 p-2 d-flex justify-content-center align-items-center">
+                <div className='flex justify-center align-center'>
+                  <div className="flex flex-col items-center   align-center">
+                    <div className='mb-12'>
+                      <img src={logo} width={100} />
                     </div>
-                    <span className="ml-4">
-                      Sign In with Google
-                    </span>
-                  </button>
-                </div>
-                <div className="my-3 border-b text-center">
-                  <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-
+                    <button className="w-full px-2 max-w-xs font-bold shadow-sm rounded-lg py-3 bg-green-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+                      <div className="bg-white p-2 rounded-full">
+                        <svg className="w-4" viewBox="0 0 533.5 544.3">
+                          <path d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z" fill="#4285f4" />
+                          <path d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z" fill="#34a853" />
+                          <path d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z" fill="#fbbc04" />
+                          <path d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z" fill="#ea4335" />
+                        </svg>
+                      </div>
+                      <span className="ml-4">
+                        Sign In with Google
+                      </span>
+                    </button>
                   </div>
                 </div>
-                <form action="" onSubmit={handleSubmits}>
-                  <div className="mx-auto max-w-xs">
+
+              </div>
+              <div className="col-12 col-lg-6 ">
+                <div className=' bg-green-100 text-center hidden lg:flex  flex justify-center align-center'>
+                  <div className="m-16 bg-cover bg-center bg-no-repeat " >
+                    <img src="https://readymadeui.com/signin-image.webp" width={300} alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className=" border-b text-center">
+              <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+
+              </div>
+            </div>
+
+            <form action="" onSubmit={handleSubmits} className='p-4'>
+              <div className="col-12 d-flex flex-wrap">
+                <div className="col-12 col-lg-6">
+                  <div className='px-2'>
                     <input
                       type="text"
                       value={name}
@@ -81,45 +119,75 @@ function Registration() {
                       value={confirmpassword}
                       onChange={(e) => setConfirmpassword(e.target.value)}
                       className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-3" type="password" placeholder="Confirm Password" />
+                  </div>
+                </div>
+                <div className="col-12 col-lg-6">
+                  <div className='px-2'>
                     <input
                       value={phone}
                       onChange={(e) => setPhone(e.target.value)}
                       className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-3" type="text" placeholder="Contect" />
+                    <div className="col-12 d-flex flex-wrap">
+                      <div className="col-12 col-lg-6 p-1">
+                        <input
+                          value={area}
+                          onChange={(e) => setArea(e.target.value)}
+                          className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-3" type="text" placeholder="Area" />
+                      </div>
+                      <div className="col-12 col-lg-6 p-1">
+                        <input
+                          value={city}
+                          onChange={(e) => setCity(e.target.value)}
+                          className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-3" type="text" placeholder="City" />
+                      </div>
+                      <div className="col-12 col-lg-6 p-1">
+                        <input
+                          value={state}
+                          onChange={(e) => setState(e.target.value)}
+                          className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2" type="text" placeholder="State" />
+                      </div>
+                      <div className="col-12 col-lg-6 p-1">
+                        <input
+                          value={country}
+                          onChange={(e) => setCountry(e.target.value)}
+                          className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2" type="text" placeholder="Country" />
+                      </div>
+                      <div className="col-12 col-lg-6 p-1">
+                        <input
+                          value={pincode}
+                          onChange={(e) => setPincode(e.target.value)}
+                          className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-2" type="text" placeholder="Pincode" />
+                      </div>
+                    </div>
 
-                    <input
-                      value={address}
-                      onChange={(e) => setAddress(e.target.value)}
-                      className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-3" type="text" placeholder="Address" />
-
-                    <button type='submit' className="mt-3 tracking-wide font-semibold bg-green-600 text-white w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                      <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
-                        <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                        <circle cx="8.5" cy={7} r={4} />
-                        <path d="M20 8v6M23 11h-6" />
-                      </svg>
-                      <span className="ml-4">
-                        Sign Up Next
-                      </span>
-                    </button>
-                    <p className="mt-4 text-sm text-center text-gray-600">
-                      Allready have an account? <Link to={'/login'} className="text-success text-bold hover:underline">login </Link>
-                    </p>
                   </div>
-                </form>
+                </div>
+
               </div>
-            </div>
+
+              <div className='d-flex justify-content-center'>
+                <button type='submit' className="mt-3 tracking-wide font-semibold bg-green-600 text-white p-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                  <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                    <circle cx="8.5" cy={7} r={4} />
+                    <path d="M20 8v6M23 11h-6" />
+                  </svg>
+                  <span className="ml-4">
+                    Sign Up Next
+                  </span>
+                </button>
+              </div>
+              <p className="mt-4 text-sm text-center text-gray-600">
+                Allready have an account? <Link to={'/login'} className="text-success text-bold hover:underline">login </Link>
+              </p>
+            </form>
+
           </div>
-          <div className="flex-1 bg-green-100 text-center hidden lg:flex">
-            <div className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat" >
-              <img src="https://readymadeui.com/signin-image.webp" alt="" />
-            </div>
-          </div>
+
         </div>
       </div>
-          </div>
-        </div>
-        </>
-    )
+    </>
+  )
 }
 
 export default Registration
