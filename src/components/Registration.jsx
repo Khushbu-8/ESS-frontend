c47908eb989f5bf82afbe6bfdd445f4e7943f191
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-14 18:41:03 +0530

    send

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -208,7 +208,7 @@ function Registration() {
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                       className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-3" type="password" placeholder="Password" />
-                    {errors.password && <span className="error text-orange text-orange text-sm">{errors.password}</span>}
+                    {errors.password || <span className="error text-orange text-orange text-sm">{errors.password}</span>}
                     <input
                       value={confirmpassword}
                       onChange={(e) => setConfirmpassword(e.target.value)}
