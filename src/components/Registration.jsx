9640502041053b8b6282de4532d9c162d7eb3759
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-14 18:47:34 +0530

    send

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -203,12 +203,13 @@ function Registration() {
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-3" placeholder="Email" />
+                      <span className='text-gray text-sm'>Email must end with @gmail.com.</span>
                     {errors.email && <span className="error text-orange text-sm">{errors.email}</span>}
                     <input
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                       className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-3" type="password" placeholder="Password" />
-                    {errors.password || <span className="error text-orange text-orange text-sm">{errors.password}</span>}
+                    {errors.password && <span className="error text-orange text-orange text-sm">{errors.password}</span>}
                     <input
                       value={confirmpassword}
                       onChange={(e) => setConfirmpassword(e.target.value)}
