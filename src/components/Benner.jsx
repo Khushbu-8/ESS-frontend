730ca7932f055853deffcde4592ab8c03cfa420a
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-17 17:41:50 +0530

    category

--- src/components/Benner.jsx
+++ src/components/Benner.jsx
@@ -17,7 +17,7 @@ const Benner = () => {
     ]
     return (
         <>
-            <section className='mt-5'>
+            <section className='my-16'>
                 <div className="container">
                     <div className="row">
                         <div className="col-12 d-flex flex-wrap">
--- src/components/ServieceCategories.jsx
+++ src/components/ServieceCategories.jsx
@@ -9,29 +9,7 @@ const  ServieceCategories = ({ categories }) => {
 
     return (
         <>
-        <section>
-        <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="col-4">
-              <label className=" px-3  border  flex items-center gap-2 ">
-                <input type="text" className="grow outline-none  bg-base-100 p-2 "  placeholder="Search" />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  fill="currentColor"
-                  className="h-4 w-4 opacity-70">
-                  <path
-                    fillRule="evenodd"
-                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                    clipRule="evenodd" />
-                </svg>
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
-        </section>
+      
         <section className='mt-2'>
             <div className="container">
                 <div className="row row-cols-3 row-cols-lg-5 g-lg-3">
