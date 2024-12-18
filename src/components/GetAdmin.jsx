d4f77236bc0e629194481e01c20a273ddbfab910
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-18 15:19:55 +0530

    serviece

--- src/components/GetAdmin.jsx
+++ src/components/GetAdmin.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaUserAltSlash } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
