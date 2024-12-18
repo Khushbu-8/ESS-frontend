c3422bd5619b534050afa722eff835789c5a8ba2
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-18 15:10:32 +0530

    serviece

--- src/components/GetAdmin.jsx
+++ src/components/GetAdmin.jsx
@@ -8,7 +8,7 @@ const GetAdmin = () => {
             <li className=' p-2 rounded hover:bg-primary hover:text-white focus:text-white '>
                 <Link  className=' text-lg d-flex align-items-center'>
                     <span className='inline-block mr-2 text-xl '><FaUserAltSlash /></span>
-                    Addmin</Link>
+                    Admin</Link>
             </li>
         </>
     )
