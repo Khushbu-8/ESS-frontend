a584eb48aaede599a07b6b17e179ed4c1ea6e4c6
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-20 17:56:37 +0530

    edit

--- src/components/Senedrequest.jsx
+++ src/components/Senedrequest.jsx
@@ -16,7 +16,7 @@ const Senedrequest = ({sendedRequest}) => {
     
     const cancleRequest = async(senderId) => {
         try {
-            const response = await axios.post(`${backend_API}/request/cancelRequest`, { senderId }, // Request payload
+            const response = await axios.post(`${backend_API}/request/cancelRequest`, { senderId : senderId }, // Request payload
                 {
                     headers: {
                         "Content-Type": "application/json",
